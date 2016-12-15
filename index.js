d73d53dfab62866a4b803b84d85e865644a543d3
Jay <jcoug1294@gmail.com>
2016-12-15 18:10:21 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ function repeatedlySendRequest() {
 		repeatedlySendRequest(); 
 	}); 
 }
+
+function sendChatEntranceRequest(username) {   
+	var req = new XMLHttpRequest();   
+	req.open("GET", "http://localhost:9000/?user=" + username, true);  
+	req.send(null); // send request w/no request body (used for POST requests)  
+	req.addEventListener("load", function() { 
+		console.log(req.responseText);
+		repeatedlySendRequest();
+	}); 
+}
+
+function sendMessage(message){
+	document.getElementById("msgSubmitButton").addEventListener("click", function() { 
+ 		var msg      = document.getElementById("messageField").value; 
+  	 	var username = document.getElementById("hiddenField").value; // other options? 
+     	sendMessage(username, message); // implement like sendChatEntranceRequest,                                   // refactoring?
+		repeatedlySendRequest();
+	});
+}
